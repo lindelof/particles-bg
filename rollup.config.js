977657ee2a6ae5cbb584c6e9ba0aad835f8c96a5
Dragoș Străinu <str.dr4605@gmail.com>
2019-12-09 00:03:02 +0200

    Add typing, gitignore dist folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import copy from 'rollup-plugin-copy'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
@@ -22,8 +23,11 @@ export default {
       sourcemap: true
     }
   ],
-  external: ['react', 'proton-engine','raf-manager'],   
+  external: ['react', 'proton-engine','raf-manager'],
   plugins: [
+    copy({
+      targets: [{ src: 'src/index.d.ts', dest: 'dist' }]
+    }),
     external(),
     postcss({
       modules: true
